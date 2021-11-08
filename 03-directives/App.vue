544196d989de05fcefb72d28a229992a9f6d6690
Luann Rodrigo <luann.rodrigos@gmail.com>
2021-11-08 15:32:54 -0300

    directives

---
+++ 03-directives/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>My task list</h1>
+    <button @click="() => showList = !showList">Show list</button>
+    <br />
+    <input type="text" v-focus>
+    <ul v-if="showList">
+        <li 
+            v-for="(task, index) in tasks"
+            :key="`${task}-${index}`"
+        >
+            {{ task.name }}
+        </li>
+    </ul>
+    <p v-else>Anonimous list task</p>
+  </div>
+</template>
+
+<script>
+const focus = {
+    inserted: (el) => {
+        el.focus()
+    }
+}
+export default {
+    directives: {
+        focus
+    },
+    data: () => ({
+        showList: false,
+        tasks: [{name: 'Curso', isDone: false}],
+    })
+}
+</script>
+
+<style>
+
+</style>
