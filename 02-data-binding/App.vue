214d849023f55d765e214cf8c5d7ce0503dedd28
Luann Rodrigo <luann.rodrigos@gmail.com>
2021-11-08 15:14:46 -0300

    data-binding

---
+++ 02-data-binding/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1 v-bind:style="{textDecoration: decoration}"> hello world {{ name }}</h1>
+        <input type="text" v-model="name" />
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        name: 'luann',
+        decoration: 'underline'
+    })
+}
+</script>
+
+<style>
+
+</style>
